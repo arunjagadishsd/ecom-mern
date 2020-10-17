843f94a41f306eaeaf53399d63bd9d95db1fc3f1
AJ <arunjagadishsd@gmail.com>
2020-10-17 12:28:23 +0530

    Add Checkout steps

--- frontend/src/pages/ShippingPage.js
+++ frontend/src/pages/ShippingPage.js
@@ -2,18 +2,28 @@ import React, { useState } from "react";
 import { Form, Button } from "react-bootstrap";
 import { useDispatch, useSelector } from "react-redux";
 import FormContainer from "../components/FormContainer";
+import CheckoutSteps from "../components/CheckoutSteps";
+import { saveShippingAddress } from "../actions/cartAction";
 
 const ShippingPage = ({ history }) => {
-  const [address, setAddress] = useState("");
-  const [city, setCity] = useState("");
-  const [postalCode, setPostalCode] = useState("");
-  const [country, setCountry] = useState("");
+  const cart = useSelector((state) => state.cart);
+  const { shippingAddress } = cart;
+
+  const [address, setAddress] = useState(shippingAddress.address);
+  const [city, setCity] = useState(shippingAddress.city);
+  const [postalCode, setPostalCode] = useState(shippingAddress.postalCode);
+  const [country, setCountry] = useState(shippingAddress.country);
+
+  const dispatch = useDispatch();
+
   const submitHandler = (e) => {
     e.preventDefault();
-    console.log("submit");
+    dispatch(saveShippingAddress({ address, city, postalCode, country }));
+    history.push("/payment");
   };
   return (
     <FormContainer>
+      <CheckoutSteps step1 step2 />
       <h1>Shipping</h1>
       <Form onSubmit={submitHandler}>
         <Form.Group>
@@ -47,10 +57,10 @@ const ShippingPage = ({ history }) => {
           ></Form.Control>
         </Form.Group>
         <Form.Group>
-          <Form.Label>Address</Form.Label>
+          <Form.Label>Country</Form.Label>
           <Form.Control
             type="text"
-            placeholder="Enter Adress"
+            placeholder="Enter Country"
             value={country}
             required
             onChange={(e) => setCountry(e.target.value)}
